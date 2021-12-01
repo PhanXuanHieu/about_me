e20d1cb89adf96bfbb58a84669e9ec8f728a20b0
Hiếu <60700486+PhanXuanHieu@users.noreply.github.com>
2021-12-01 22:25:23 +0700

    Add files via upload

---
+++ js/typeWriter.js
@@ -0,0 +1,37 @@
+
+let i = 0;
+let text1 = "Hey! What's up.";
+let text2 = "Bạn tìm kiếm thông tin về mình ư? Vậy click dưới đi này. ";
+let speed = 40;
+
+function typeWriter(text, para){
+	if(ok == 2){
+		clearInterval(typeInterval);
+	}
+	if(i < text.length){
+		document.getElementById(para).innerHTML += text.charAt(i);
+		i++;
+		speed = Math.random() * 50 + 100;
+	}
+	else{
+		if(ok == 0){
+			i = 0;
+		}
+		ok += 1;
+	}
+}
+
+var typeInterval;
+
+//window.onload = function() {
+//	window.onload = function(){};
+   	typeInterval = setInterval(function(){
+		if(ok == 0){
+			typeWriter(text1, "txt1");
+		}
+		else if(ok == 1){
+			typeWriter(text2, "txt2");
+
+		}
+	}, 100);
+//};
